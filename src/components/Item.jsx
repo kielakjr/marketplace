ba3e78bce21692223593c8ae128b798e2f8bd65a
mkielak <kielakowsky17@gmail.com>
2025-12-13 21:05:25 +0100

    Added basic styling for items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Item = ( { item }) => {
   return (
     <div className="bg-white p-4 h-full rounded-md shadow-md aspect-square">
-      <img src={item.images[0]} alt={item.name} className="w-full h-3/4 object-cover rounded-md" />
-      <h3>{item.name}</h3>
-      <p>{item.price} {item.currency}</p>
+      <img src={item.images[0]} alt={item.name} className="w-full object-cover rounded-md" />
+      <h3 className="text-lg font-semibold mt-2 md:text-xl">{item.name}</h3>
+      <p className="mr-auto">{item.price} {item.currency}</p>
     </div>
   )
 }
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -4,7 +4,7 @@ import Item from './Item'
 
 const Items = () => {
   return (
-    <div className="overflow-auto grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
+    <div className="overflow-auto overflow-x-hidden grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4 mx-3">
       {items.map((item) => (
         <Item key={item.id} item={item} />
       ))}
