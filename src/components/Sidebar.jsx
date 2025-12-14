4339e23ac72e11c77d9f49c5f118eecdb1c46652
mkielak <kielakowsky17@gmail.com>
2025-12-14 11:48:15 +0100

    Added collapsing sidebar for mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { categories, sellers, locations } from '../data/items'
 import { useState } from 'react'
+import { GoSidebarCollapse, GoSidebarExpand  } from "react-icons/go";
 
 const sections = {
   "Kategorie": categories,
@@ -11,6 +12,7 @@ const sections = {
 }
 
 const Sidebar = ( { onSave }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   const [filters, setFilters] = useState({});
 
@@ -36,7 +38,15 @@ const Sidebar = ( { onSave }) => {
   }
 
   return (
-    <aside className="w-1/3 h-[90vh] overflow-y-auto md:w-1/4 lg:w-1/5 bg-white rounded-md shadow-md ml-2 p-4 sticky top-11 left-0">
+    <>
+    <button
+      className={`fixed top-16 ${isCollapsed ? "-left-2" : "left-36"} z-50 bg-white p-2 rounded-md shadow-md md:hidden`}
+      onClick={() => setIsCollapsed(!isCollapsed)}
+    >
+      {!isCollapsed ? <GoSidebarExpand size={24} /> : <GoSidebarCollapse size={24} />}
+    </button>
+    {isCollapsed ? null :
+    <aside className="w-full h-[90vh] overflow-y-auto md:w-1/4 lg:w-1/5 bg-white rounded-md shadow-md ml-2 p-4 sticky top-11 left-0">
       {sections && Object.entries(sections).map(([sectionTitle, options]) => (
         <div key={sectionTitle} className="mb-6">
           <h2 className="text-lg font-semibold mb-3">{sectionTitle}</h2>
@@ -44,7 +54,7 @@ const Sidebar = ( { onSave }) => {
             {options.map((option) => (
               <li key={option} className="mb-2">
                 <label className="inline-flex items-center">
-                  <input type="checkbox" className="form-checkbox h-4 w-4 text-blue-700" onChange={() => handleFilterChange(sectionTitle, option)} />
+                  <input checked={filters[sectionTitle]?.includes(option)} type="checkbox" className="form-checkbox h-4 w-4 text-blue-700" onChange={() => handleFilterChange(sectionTitle, option)} />
                   <span className="ml-2">{option}</span>
                 </label>
               </li>
@@ -54,6 +64,8 @@ const Sidebar = ( { onSave }) => {
       ))}
       <button onClick={handleSave} className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition-colors">Zastosuj filtry</button>
     </aside>
+    }
+    </>
   )
 }
 
